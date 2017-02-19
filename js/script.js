68191aae0c6e517238d3e4a6d4c294d98b458502
Pampushka-Lju <pampushka-lju@mail.ru>
2017-02-19 18:49:02 +0500

    доработка5

--- js/script.js
+++ js/script.js
@@ -3,5 +3,19 @@ var popup = document.querySelector(".feedback-form");
 var overlay = document.querySelector(".overlay");
 var close = popup.querySelector(".feedback-form-close");
 
-var name = popup.querySelector("[name=name]");
-var mail = popup.querySelector("[name=feedback-form-mail]");
+
+open.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("feedback-form-show");
+        overlay.classList.add("overlay-show");
+
+window.addEventListener("keydown", function(event) {
+       if (event.keyCode === 27) {
+        if (popup.classList.contains("feedback-form-show")) {
+            overlay.classList.remove("overlay-show");
+          }}});
+
+close.addEventListener("click", function(event) {
+        event.preventDefault();
+        popup.classList.add("feedback-form-show");
+        overlay.classList.add("overlay-show");
