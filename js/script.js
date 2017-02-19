e92c41babe2ae36b3d3af7dd3eaf531d205de6bc
Pampushka-Lju <pampushka-lju@mail.ru>
2017-02-19 15:39:34 +0500

    Доработка4

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var open = document.querySelector(".feedback-form .btn");
+var popup = document.querySelector(".feedback-form");
+var overlay = document.querySelector(".overlay");
+var close = popup.querySelector(".feedback-form-close");
+
+var name = popup.querySelector("[name=name]");
+var mail = popup.querySelector("[name=feedback-form-mail]");
