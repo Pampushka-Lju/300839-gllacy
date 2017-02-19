87819dffe5e3c8675324bf6f49b609fa9f8afa73
Pampushka-Lju <pampushka-lju@mail.ru>
2017-02-19 23:07:10 +0500

    Доработка6

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var open = document.querySelector(".feedback-form .btn");
+var open = document.querySelector(".contacts .btn");
 var popup = document.querySelector(".feedback-form");
 var overlay = document.querySelector(".overlay");
 var close = popup.querySelector(".feedback-form-close");
@@ -7,15 +7,17 @@ var close = popup.querySelector(".feedback-form-close");
 open.addEventListener("click", function(event) {
         event.preventDefault();
         popup.classList.add("feedback-form-show");
-        overlay.classList.add("overlay-show");
+        overlay.classList.add("overlay-show")});
 
 window.addEventListener("keydown", function(event) {
        if (event.keyCode === 27) {
         if (popup.classList.contains("feedback-form-show")) {
+            popup.classList.remove("feedback-form-show");
             overlay.classList.remove("overlay-show");
           }}});
 
 close.addEventListener("click", function(event) {
         event.preventDefault();
-        popup.classList.add("feedback-form-show");
-        overlay.classList.add("overlay-show");
+        popup.classList.remove("feedback-form-show");
+        overlay.classList.remove("overlay-show");
+      });
